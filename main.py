371facae6d00647246a64856053bc962272a6e4a
smilingly1919 <pengfan_chen@foxmail.com>
2025-07-28 22:52:40 +0800

    完善

--- main.py
+++
@@ -1,3 +0,0 @@
-from sdf_utils import a
-python -m sdf_utils
-print(a(10))
--- sdf_utils/__init__.py
+++ sdf_utils/__init__.py
@@ -1,1 +1,5 @@
-from .io_utils import a
+from .io_utils import load_field,load_density,load_idall
+
+__all__ = [
+    "a",
+]
